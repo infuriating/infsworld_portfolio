9a4455859e50c2c67c51385ef7993e814bb68662
inf <infuriatingx1@gmail.com>
2023-08-29 14:32:20 +0200

    touched up styling, added mobile menu

--- js/nav.js
+++ js/nav.js
@@ -22,23 +22,34 @@ function setActivePage() {
       }, key);
     }
   });
+}
 
-  // navLinks.forEach((link) => {
-  //   if (urlString === NavPages[url]) {
-  //     link.classList.add("nav-active");
-  //   }
-  // });
+function handleNavMenu() {
+  const navMenu = document.querySelector(".nav-menu");
+
+  if (navMenu.classList.contains("nav-menu_active")) {
+    navMenu.style.transform = "translateX(100%)";
+    setTimeout(() => {
+      navMenu.classList.toggle("nav-menu_active");
+    }, 375);
+  } else {
+    navMenu.classList.toggle("nav-menu_active");
+    navMenu.style.transform = "translateX(0%)";
+  }
 }
 
 function loadNav() {
   const nav = document.querySelector("nav");
-  // add this at last
-  // <div class="nav-menu">
-  //   <div class="nav-menu_line"></div>
-  //   <div class="nav-menu_line"></div>
-  //   <div class="nav-menu_line"></div>
-  // </div>
   nav.innerHTML = `
+    <div class="nav-menu">
+      <div class="nav-menu_button" onclick="handleNavMenu()"></div>
+      <div class="nav-menu_items">
+        <a href="${NavPages.about}" class="nav-menu_link">About</a>
+        <a href="${NavPages.contact}" class="nav-menu_link">Contact</a>
+        <a href="${NavPages.projects}" class="nav-menu_link">Projects</a>
+      </div>
+    </div>
+    <div class="nav-menu_button" onclick="handleNavMenu()"></div>
     <div class="nav-container">
       <div class="nav-logo">
         <a href="${NavPages.home}">

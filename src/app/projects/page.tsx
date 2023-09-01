596e5d1f4afaedea2cdb121bef5fe32044b58473
inf <infuriatingx1@gmail.com>
2023-09-01 13:26:12 +0200

    added some styling to the projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,7 +12,7 @@ export default async function Index() {
 
   return (
     <PageContainer>
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 place-items-center font-poppins">
+      <div className="grid grid-cols-1 gap-4 xl:grid-cols-2 place-items-center font-poppins">
         {projects?.map((project) => (
           <div
             key={project.id}
@@ -24,13 +24,13 @@ export default async function Index() {
               <span className="font-bold">{project.date.substring(10, 0)}</span>
             </p>
             <Image
-              className="rounded-lg"
+              className="rounded-lg blur-sm saturate-0 hover:blur-none hover:saturate-100 transition-all duration-500"
               src={`/images/projects/${project.image}.png`}
               alt={project.title}
               width={500}
               height={300}
             />
-            <p className="font-bold text-2xl">{project.title}</p>
+            <p className="font-bold text-2xl py-1">{project.title}</p>
             <p>{project.information}</p>
             <p className="text-lg font-semibold">Tools used:</p>
             <div className="flex gap-1">

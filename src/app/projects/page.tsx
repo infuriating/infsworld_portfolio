2dd1825a5ad1b8ec7c3df8556e80887ffdfc31a4
inf <decimatedxd@gmail.com>
2023-09-01 09:09:10 +0200

    rendered projects via Supabase

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,52 @@
 import PageContainer from "@/components/PageContainer";
-import React from "react";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-export default function page() {
+import Image from "next/image";
+
+export default async function Index() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data: projects } = await supabase.from("projects").select("*");
+
+  // columns: id, date (timestamp), title, description, image (name but lowercase), tools (text[])
+
   return (
     <PageContainer>
-      <p className="text-4xl font-bold">Hey!</p>
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 place-items-center font-poppins">
+        {projects?.map((project) => (
+          <div
+            key={project.id}
+            className="bg-primary px-8 py-4 flex flex-col gap-2 justify-center max-w-3xl rounded-lg relative"
+          >
+            <div className="absolute bg-neutral-900 w-full h-full blur-2xl left-0 top-0 -z-10"></div>
+            <p className="text-neutral-400 text-sm">
+              Made at{" "}
+              <span className="font-bold">{project.date.substring(10, 0)}</span>
+            </p>
+            <Image
+              className="rounded-lg"
+              src={`/images/projects/${project.image}.png`}
+              alt={project.title}
+              width={500}
+              height={300}
+            />
+            <p className="font-bold text-2xl">{project.title}</p>
+            <p>{project.information}</p>
+            <p className="text-lg font-semibold">Tools used:</p>
+            <div className="flex gap-1">
+              {project.tools.map((tool: string) => (
+                <Image
+                  key={project.tools}
+                  src={`/images/tools/${tool.toLowerCase()}.png`}
+                  alt={tool}
+                  width={50}
+                  height={50}
+                />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </PageContainer>
   );
 }

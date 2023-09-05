da2b909a2829fbd9d847080073bb995029cbd786
infuriating <infuriatingx1@gmail.com>
2023-09-05 11:52:08 +0200

    progress on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import PageContainer from "@/components/PageContainer";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className="h-container-svh justify-center items-center flex flex-col">
       <div className="flex justify-center items-center gap-2 md:gap-4 mb-4">
         <p className="text-2xl md:text-5xl font-bold mb-1">Welcome to</p>
         <Image
@@ -22,10 +22,33 @@ export default function Home() {
         />
       </div>
 
-      <p>
-        My name is <b>Luca Kuiper</b> and I&apos;m a 20 year old{" "}
-        <b>Full-Stack Developer</b> from The Netherlands
-      </p>
+      <div className="flex flex-col gap-2">
+        <p className="text-center text-lg md:text-2xl mb-4">
+          My name is <b>Luca Kuiper</b> and I&apos;m a 20 year old{" "}
+          <b>Full-Stack Developer</b> from The Netherlands
+        </p>
+        <p className="text-center text-md md:text-xl">
+          This website was built to showcase my skills, projects and show you
+          who I am!
+        </p>
+        <p className="text-center text-md md:text-xl">
+          Feel free to learn more about me by pressing the buttons below!
+        </p>
+        <div className="flex gap-4 justify-center items-center mt-4">
+          <Link
+            href="/about"
+            className="font-semibold bg-blue-400 px-8 py-2 rounded-lg text-neutral-100 transition-all duration-200 hover:bg-blue-500"
+          >
+            About me
+          </Link>
+          <Link
+            href="/projects"
+            className="font-semibold bg-red-300 px-8 py-2 rounded-lg transition-all duration-200 hover:bg-red-400"
+          >
+            Projects
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }

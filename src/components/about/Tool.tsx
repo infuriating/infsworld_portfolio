241fc2b902fb5b33ea14fab204429c4f09bbf718
inf <decimatedxd@gmail.com>
2023-09-06 10:50:19 +0200

    added tooltips to the tools

--- src/components/about/Tool.tsx
+++ src/components/about/Tool.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { NextUIProvider, Tooltip } from "@nextui-org/react";
 import React, { useState } from "react";
 import { Tools } from "@/lib/LocalFetch";
 import Image from "next/image";
@@ -8,7 +9,7 @@ export default function Tool() {
   const [carouselOpened, setCarouselOpened] = useState(false);
 
   return (
-    <>
+    <NextUIProvider>
       <p className="text-2xl font-semibold mt-8 text-center">
         Here you can see the tools that I currently use, or am familiar with.
       </p>
@@ -26,13 +27,18 @@ export default function Tool() {
             <p className="text-xl font-bold text-center mb-2">Technologies</p>
             <div className="flex gap-1 flex-wrap justify-center items-center">
               {Tools.Technologies.map((technology) => (
-                <Image
+                <Tooltip
                   key={technology.name}
-                  src={`/images/tools/${technology.name.toLowerCase()}.png`}
-                  alt={technology.name}
-                  width={50}
-                  height={50}
-                />
+                  color="primary"
+                  content={technology.name}
+                >
+                  <Image
+                    src={`/images/tools/${technology.name.toLowerCase()}.png`}
+                    alt={technology.name}
+                    width={50}
+                    height={50}
+                  />
+                </Tooltip>
               ))}
             </div>
           </div>
@@ -40,13 +46,18 @@ export default function Tool() {
             <p className="text-xl font-bold text-center mb-2">Frameworks</p>
             <div className="flex gap-1 flex-wrap justify-center items-center">
               {Tools.Frameworks.map((framework) => (
-                <Image
+                <Tooltip
                   key={framework.name}
-                  src={`/images/tools/${framework.name.toLowerCase()}.png`}
-                  alt={framework.name}
-                  width={50}
-                  height={50}
-                />
+                  color="primary"
+                  content={framework.name}
+                >
+                  <Image
+                    src={`/images/tools/${framework.name.toLowerCase()}.png`}
+                    alt={framework.name}
+                    width={50}
+                    height={50}
+                  />
+                </Tooltip>
               ))}
             </div>
           </div>
@@ -54,13 +65,18 @@ export default function Tool() {
             <p className="text-xl font-bold text-center mb-2">Libraries</p>
             <div className="flex gap-1 flex-wrap justify-center items-center">
               {Tools.Libraries.map((library) => (
-                <Image
+                <Tooltip
                   key={library.name}
-                  src={`/images/tools/${library.name.toLowerCase()}.png`}
-                  alt={library.name}
-                  width={50}
-                  height={50}
-                />
+                  color="primary"
+                  content={library.name}
+                >
+                  <Image
+                    src={`/images/tools/${library.name.toLowerCase()}.png`}
+                    alt={library.name}
+                    width={50}
+                    height={50}
+                  />
+                </Tooltip>
               ))}
             </div>
           </div>
@@ -68,18 +84,23 @@ export default function Tool() {
             <p className="text-xl font-bold text-center mb-2">Databases</p>
             <div className="flex gap-1 flex-wrap justify-center items-center">
               {Tools.Databases.map((database) => (
-                <Image
+                <Tooltip
                   key={database.name}
-                  src={`/images/tools/${database.name.toLowerCase()}.png`}
-                  alt={database.name}
-                  width={50}
-                  height={50}
-                />
+                  color="primary"
+                  content={database.name}
+                >
+                  <Image
+                    src={`/images/tools/${database.name.toLowerCase()}.png`}
+                    alt={database.name}
+                    width={50}
+                    height={50}
+                  />
+                </Tooltip>
               ))}
             </div>
           </div>
         </div>
       )}
-    </>
+    </NextUIProvider>
   );
 }
